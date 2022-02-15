652fd9f3eb26d6baa306bfa2c54dfbef86a52618
Bill Cauchois <wcauchois@gmail.com>
2022-02-15 01:55:08 -0500

    wasm loading moved into react

--- src/web/main.tsx
+++ src/web/main.tsx
@@ -1,13 +1,30 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import ReactDOM from "react-dom";
 
-// import * as foo from "../../bazel-bin/src/engine/engine_lib_wasm_bindgen";
 import init, { double } from "engine_lib_wasm_bindgen";
 
-function App() {
+function Main() {
   return (
     <div>
-      Hello, world...
+      Hello world, the double is {double(2)}
+    </div>
+  )
+}
+
+function App() {
+  const [initialized, setInitialized] = useState(false);
+
+  useEffect(() => {
+    async function doInit() {
+      await init("../engine/engine_lib_wasm_bindgen_bg.wasm");
+      setInitialized(true);
+    }
+    doInit();
+  }, []);
+
+  return (
+    <div>
+      {initialized && <Main />}
     </div>
   );
 }
@@ -18,12 +35,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById("root")
 );
-
-async function runFoo() {
-  console.log('run foo?');
-  await init("../engine/engine_lib_wasm_bindgen_bg.wasm");
-  console.log('done?');
-
-  console.log('double is', double(2));
-}
-runFoo();
