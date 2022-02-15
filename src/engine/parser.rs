779c29cee509833704f92f503c3c9eef66dfecf4
Bill Cauchois <wcauchois@gmail.com>
2022-02-15 16:16:23 -0500

    implement float parsing

--- src/engine/parser.rs
+++ src/engine/parser.rs
@@ -7,23 +7,26 @@ use nom::{
     combinator::{cut, map, map_res, opt},
     error::{context, VerboseError},
     multi::many0,
+    number::complete::float,
     sequence::{delimited, preceded, terminated, tuple},
     IResult, Parser,
 };
 
 #[derive(Debug, PartialEq, Clone)]
 pub enum Expr {
-    Number(f64),
+    Number(f32),
     String(String),
     Symbol(String),
     List(Vec<Expr>),
 }
 
-fn parse_number<'a>(input: &'a str) -> IResult<&'a str, Expr, VerboseError<&'a str>> {
-    map_res(digit1, |s: &str| s.parse::<f64>().map(|n| Expr::Number(n)))(input)
+type ExprParseResult<'a> = IResult<&'a str, Expr, VerboseError<&'a str>>;
+
+fn parse_number<'a>(input: &'a str) -> ExprParseResult<'a> {
+    map(float, |n| Expr::Number(n))(input)
 }
 
-fn parse_expr<'a>(input: &'a str) -> IResult<&'a str, Expr, VerboseError<&'a str>> {
+fn parse_expr<'a>(input: &'a str) -> ExprParseResult {
     preceded(
         multispace0,
         // alt((parse_number, parse_string, parse_symbol, parse_list)),
@@ -42,7 +45,12 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_parse_number() {
+    fn test_int_number() {
         assert_eq!(parse("42"), Ok(Expr::Number(42.0)));
+    }
+
+    #[test]
+    fn test_float_number() {
+        assert_eq!(parse("1.234"), Ok(Expr::Number(1.234)));
     }
 }
