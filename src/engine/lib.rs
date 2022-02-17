aa46bfd0316c35574cc67a732163afdf8fefd3d2
Bill Cauchois <wcauchois@gmail.com>
2022-02-17 14:44:36 -0500

    start fleshing out sheet interface, use interpreter, etc

--- src/engine/lib.rs
+++ src/engine/lib.rs
@@ -6,6 +6,7 @@ extern crate lazy_static;
 
 use std::cell::Cell;
 use std::collections::HashMap;
+use std::fmt;
 use wasm_bindgen::prelude::*;
 
 mod dep_graph;
@@ -13,13 +14,18 @@ mod error;
 mod interpreter;
 mod parser;
 
+use dep_graph::DepGraph;
+use error::AppResult;
+use interpreter::{eval, Env};
+use parser::{interpret_cell, InterpretCellResult};
+
 #[derive(Eq, PartialEq, Hash, Clone)]
-struct SheetAddress {
+pub struct SheetAddress {
     row: i32,
     col: i32,
 }
 
-struct SheetFormula {
+pub struct SheetFormula {
     address: SheetAddress,
     program: interpreter::Program,
     references: Vec<SheetAddress>,
@@ -37,14 +43,15 @@ impl dep_graph::Node<SheetAddress> for SheetFormula {
 
 // on cell change: get dependents, recompute the computed value..
 
-enum SheetCellComputedValue {
+#[derive(Clone, Debug)]
+pub enum SheetCellComputedValue {
     Number(f32),
     Text(String),
     Invalid { message: String },
 }
 
 impl SheetCellComputedValue {
-    fn from_interpreted_value(ivalue: interpreter::Value) -> Self {
+    pub fn from_interpreter_value(ivalue: interpreter::Value) -> Self {
         match ivalue {
             interpreter::Value::Number(n) => SheetCellComputedValue::Number(n),
             interpreter::Value::String(n) => SheetCellComputedValue::Text(n),
@@ -53,36 +60,89 @@ impl SheetCellComputedValue {
             },
         }
     }
+
+    pub fn empty() -> Self {
+        Self::Text("".to_string())
+    }
 }
 
-struct SheetCellValue {
-    computed_value: Cell<SheetCellComputedValue>,
-    formula: Option<SheetFormula>,
+impl fmt::Display for SheetCellComputedValue {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Self::Number(n) => write!(f, "{}", n),
+            Self::Text(s) => write!(f, "{}", s),
+            Self::Invalid { message } => write!(f, "!INVALID: {}", message),
+        }
+    }
 }
 
 struct SheetCell {
-    value: SheetCellValue,
+    computed_value: SheetCellComputedValue,
+    formula: Option<SheetFormula>,
 }
 
-#[wasm_bindgen]
 pub struct Sheet {
     cells: HashMap<SheetAddress, SheetCell>,
+    // dep_graph: Cell<DepGraph<SheetAddress>>,
+}
+
+impl Sheet {
+    pub fn set_cell(&mut self, address: SheetAddress, contents: String) -> AppResult<()> {
+        let interpreted_cell = interpret_cell(&contents)?;
+        let new_cell = match interpreted_cell {
+            InterpretCellResult::Number(n) => SheetCell {
+                computed_value: SheetCellComputedValue::Number(n),
+                formula: None,
+            },
+            InterpretCellResult::Text(s) => SheetCell {
+                computed_value: SheetCellComputedValue::Text(s),
+                formula: None,
+            },
+            InterpretCellResult::Expr(expr) => {
+                let program = interpreter::compile(&expr)?;
+                let env = Env::with_builtins();
+                let res = eval(&program, env)?;
+                SheetCell {
+                    computed_value: SheetCellComputedValue::from_interpreter_value(res),
+                    formula: Some(SheetFormula {
+                        address: address.clone(),
+                        program,
+                        references: Vec::new(), // TODO
+                    }),
+                }
+            }
+        };
+        self.cells.insert(address, new_cell);
+        Ok(())
+    }
+
+    pub fn get_cell(&self, address: SheetAddress) -> SheetCellComputedValue {
+        self.cells
+            .get(&address)
+            .map(|cell| cell.computed_value.clone())
+            .unwrap_or(SheetCellComputedValue::empty())
+    }
+
+    pub fn new() -> Self {
+        Self {
+            cells: HashMap::new(),
+        }
+    }
 }
 
 #[wasm_bindgen]
-impl Sheet {
-    pub fn get_cell(&mut self, row: i32, col: i32) -> i32 {
-        panic!()
-        // self.cells
-        //     .get(&SheetAddress { row, col })
-        //     .map(|cell| cell.value)
-        //     .unwrap_or(0)
+pub struct JsSheet(Sheet);
+
+#[wasm_bindgen]
+impl JsSheet {
+    pub fn get_cell(&mut self, row: i32, col: i32) -> String {
+        self.0.get_cell(SheetAddress { row, col }).to_string()
     }
 
-    pub fn set_cell(&mut self, row: i32, col: i32, value: i32) {
-        panic!();
-        // self.cells
-        //     .insert(SheetAddress { row, col }, SheetCell { value });
+    pub fn set_cell(&mut self, row: i32, col: i32, contents: &str) -> Result<(), JsValue> {
+        self.0
+            .set_cell(SheetAddress { row, col }, contents.to_string())
+            .map_err(|err| JsValue::from_str(format!("{}", err).as_str()))
     }
 
     pub fn debug_parse_expr(&mut self, input: &str) -> String {
@@ -106,9 +166,6 @@ impl Sheet {
 
     pub fn new() -> Self {
         console_error_panic_hook::set_once();
-
-        Self {
-            cells: HashMap::new(),
-        }
+        JsSheet(Sheet::new())
     }
 }
--- src/web/lib/Sheet.ts
+++ src/web/lib/Sheet.ts
@@ -1,17 +1,17 @@
-import { Sheet as EngineSheet } from "engine_lib_wasm_bindgen";
+import { JsSheet } from "engine_lib_wasm_bindgen";
 
 export default class Sheet {
-  private readonly underlying: EngineSheet;
+  private readonly underlying: JsSheet;
 
   constructor() {
-    this.underlying = EngineSheet.new();
+    this.underlying = JsSheet.new();
   }
 
   getCell(row: number, col: number) {
     return this.underlying.get_cell(row, col);
   }
 
-  setCell(row: number, col: number, value: number) {
-    this.underlying.set_cell(row, col, value);
+  setCell(row: number, col: number, contents: string) {
+    this.underlying.set_cell(row, col, contents);
   }
 }
