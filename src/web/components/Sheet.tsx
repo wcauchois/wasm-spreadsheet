069b872d9ba3f9f9d8ee092a307136e48b13a52e
Bill Cauchois <wcauchois@gmail.com>
2022-03-02 21:07:21 -0500

    some simple keyboard shortcuts

--- src/web/components/Sheet.tsx
+++ src/web/components/Sheet.tsx
@@ -2,6 +2,8 @@ import SheetModel from "../lib/SheetModel";
 import React, { useReducer, useState } from "react";
 import Immutable, { Range } from "immutable";
 import SheetCell from "./SheetCell";
+import { useHotkeys } from "react-hotkeys-hook";
+import { clamp } from "../lib/util";
 
 const WIDTH = 10;
 const HEIGHT = 10;
@@ -64,6 +66,38 @@ export default function Sheet({ model }: { model: SheetModel }) {
       </div>
     ))
     .toJS();
+
+  useHotkeys(
+    "enter",
+    () => {
+      setEditingCell(focusedCell);
+    },
+    [focusedCell]
+  );
+
+  const keyToDir: Array<[key: string, dir: [number, number]]> = [
+    ["left", [-1, 0]],
+    ["right", [1, 0]],
+    ["up", [0, -1]],
+    ["down", [0, 1]],
+  ];
+
+  for (const [key, dir] of keyToDir) {
+    useHotkeys(
+      key,
+      () => {
+        if (focusedCell) {
+          setFocusedCell([
+            clamp(focusedCell[0] + dir[0], 0, WIDTH),
+            clamp(focusedCell[1] + dir[1], 0, HEIGHT),
+          ]);
+        } else {
+          setFocusedCell([0, 0]);
+        }
+      },
+      [focusedCell]
+    );
+  }
 
   return (
     <div
